7e4506b5af91884f770a8ff5376dad89e0161b2d
Devashish Patel <dev.patel8787@gmail.com>
2021-08-21 22:38:39 -0400

    state setup

--- src/Components/Steps/RecieverStep.jsx
+++ src/Components/Steps/RecieverStep.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import classNames from 'classnames/bind';
-import styles from "./Step.module.scss"
 import Button from "../Button"
+import { stateList } from './State';
+import styles from "./Step.module.scss"
 
 
 const RecieverStep = ({title,handlePrevious,handleNext, setInputValue, state, errors, setState, setErrors}) => {
@@ -32,23 +33,30 @@ const RecieverStep = ({title,handlePrevious,handleNext, setInputValue, state, er
       <div className={cx('step-form')}>
         <div className={cx('form-field')}>
           <label htmlFor="name" className={cx('label')}>Name:</label>
-          <input type="text" id="name" className={cx('input')} onChange={e=>{changeHandler('name',e.target.value)}}/>
+          <input type="text" id="name" className={cx('input')} value={state.to.name} onChange={e=>{changeHandler('name',e.target.value)}}/>
         </div>
         <div className={cx('form-field')}>
           <label htmlFor="streetAddress" className={cx('label')}>Street:</label>
-          <input type="text" id="streetAddress" className={cx('input')} onChange={e=>{changeHandler('street',e.target.value)}}/>
+          <input type="text" id="streetAddress" className={cx('input')} value={state.to.street} onChange={e=>{changeHandler('street',e.target.value)}}/>
         </div>
         <div className={cx('form-field','half')}>
           <label htmlFor="city" className={cx('label')}>City:</label>
-          <input type="text" id="city" className={cx('input')} onChange={e=>{changeHandler('city',e.target.value)}}/>
+          <input type="text" id="city" className={cx('input')} value={state.to.city} onChange={e=>{changeHandler('city',e.target.value)}}/>
         </div>
         <div className={cx('form-field','half')}>
           <label htmlFor="state" className={cx('label')}>State:</label>
-          <input type="text" id="state" className={cx('input')} onChange={e=>{changeHandler('state',e.target.value)}}/>
+          <select id="state" className={cx('input')} defaultValue={state.to.state} onChange={e=>{changeHandler('state',e.target.value)}}>
+            <option value="" selected>Select State</option>
+            {stateList.map((st,index)=>{
+              return(
+                <option value={st} key={index}>{st}</option>    
+              )
+            })}            
+          </select>
         </div>
         <div className={cx('form-field','half')}>
           <label htmlFor="zip" className={cx('label')}>Zip:</label>
-          <input type="text" id="zip" className={cx('input')} onChange={e=>{changeHandler('zip',e.target.value)}}/>
+          <input type="text" id="zip" className={cx('input')} value={state.to.zip} onChange={e=>{changeHandler('zip',e.target.value)}}/>
         </div>
       </div>
     <div className={cx('button-container')}>
--- src/Components/Steps/SenderStep.jsx
+++ src/Components/Steps/SenderStep.jsx
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import classNames from 'classnames/bind';
 import styles from "./Step.module.scss"
+import { stateList } from './State';
 import Button from "../Button"
 
 
@@ -32,23 +33,30 @@ const SenderStep = ({title,handleNext, state, errors, setState, setErrors}) => {
       <div className={cx('step-form')}>
         <div className={cx('form-field')}>
           <label htmlFor="name" className={cx('label')}>Name:</label>
-          <input type="text" id="name" className={cx('input')}  onChange={e=>{changeHandler('name',e.target.value)}}/>
+          <input type="text" id="name" className={cx('input')} value={state.from.name}  onChange={e=>{changeHandler('name',e.target.value)}}/>
         </div>
         <div className={cx('form-field')}>
           <label htmlFor="streetAddress" className={cx('label')}>Street:</label>
-          <input type="text" id="streetAddress" className={cx('input')} onChange={e=>{changeHandler('street',e.target.value)}}/>
+          <input type="text" id="streetAddress" className={cx('input')}value={state.from.street} onChange={e=>{changeHandler('street',e.target.value)}}/>
         </div>
         <div className={cx('form-field','half')}>
           <label htmlFor="city" className={cx('label')}>City:</label>
-          <input type="text" id="city" className={cx('input')}onChange={e=>{changeHandler('city',e.target.value)}}/>
+          <input type="text" id="city" className={cx('input')} value={state.from.city} onChange={e=>{changeHandler('city',e.target.value)}}/>
         </div>
         <div className={cx('form-field','half')}>
           <label htmlFor="state" className={cx('label')}>State:</label>
-          <input type="text" id="state" className={cx('input')} onChange={e=>{changeHandler('state',e.target.value)}}/>
+          <select id="state" className={cx('input')} defaultValue={state.from.state} onChange={e=>{changeHandler('state',e.target.value)}}>
+            <option value="" selected>Select State</option>
+            {stateList.map((st,index)=>{
+              return(
+                <option value={st} key={index}>{st}</option>    
+              )
+            })}            
+          </select>
         </div>
         <div className={cx('form-field','half')}>
           <label htmlFor="zip" className={cx('label')}>Zip:</label>
-          <input type="text" id="zip" className={cx('input')} onChange={e=>{changeHandler('zip',e.target.value)}}/>
+          <input type="text" id="zip" className={cx('input')} value={state.from.zip} onChange={e=>{changeHandler('zip',e.target.value)}}/>
         </div>
       </div>
     <div className={cx('button-container')}>
